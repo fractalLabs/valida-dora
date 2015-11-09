9287790962a71f583c4504e8798a147a927ed1d2
nex <andres@fractal.ai>
2015-11-09 16:41:08 -0600

    first test

--- test/valida_dora/core_test.clj
+++ test/valida_dora/core_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [valida-dora.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest single-validation
+  (testing "Can I make a simple file validation?"
+    (is (= (validate "project.clj")
+           '({:meta "head -n 1", :data "(defproject valida-dora \"0.1.0-SNAPSHOT\"\n"}
+             {:meta "file", :data "project.clj: ASCII text\n"}
+             {:meta "wc -l", :data "       9 project.clj\n"})))))
